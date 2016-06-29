9210552f1998f05e49dd10f5be18d5466a6b53d3
Pau Amich <pamich1@gmail.com>
2016-06-29 22:20:35 +0200

    TOGGLE

--- js/init_map.js
+++ js/init_map.js
@@ -1,5 +1,3 @@
-
- 
         var map = L.map('map').setView([50.85, -8.04], 2);
         L.tileLayer('img/map/{z}/{y}-{x}.jpeg', {
             minZoom: 0,
@@ -29,30 +27,226 @@
 		
 
 		
-		
-		//Loop through the markers (declared on marker.js) array and add markers to layer
-		var arrMarkers = []
-         for (var i=0; i<markers.length; i++) {
+		//Loop through the SL markers (declared on marker.js) array and add markers to layer
+		var arrSLMarkers = []
+         for (var i=0; i<markers_SL.length; i++) {
            
-            var lat = markers[i][0];
-            var lon = markers[i][1];
-			var icn = markers[i][2];
-            var popupText = markers[i][3];
+            var lat = markers_SL[i][0];
+            var lon = markers_SL[i][1];
+			var icn = markers_SL[i][2];
+            var popupText = markers_SL[i][3];
             
              var marker = new L.Marker([lat, lon], {icon: icn}).bindPopup(popupText);
-			arrMarkers.push(marker)
+			arrSLMarkers.push(marker)
          }
-		var markerLayer = L.layerGroup(arrMarkers).addTo(map);
+		var markerLayerSL = L.layerGroup(arrSLMarkers).addTo(map);
+		
+		
+		//Loop through the CL markers (declared on marker.js) array and add markers to layer
+		var arrCLMarkers = []
+         for (var i=0; i<markers_CL.length; i++) {
+           
+            var lat = markers_CL[i][0];
+            var lon = markers_CL[i][1];
+			var icn = markers_CL[i][2];
+            var popupText = markers_CL[i][3];
+            
+             var marker = new L.Marker([lat, lon], {icon: icn}).bindPopup(popupText);
+			arrCLMarkers.push(marker)
+         }
+		var markerLayerCL = L.layerGroup(arrCLMarkers).addTo(map);
 
+		//Loop through the Vale markers (declared on marker.js) array and add markers to layer
+		var arrValeMarkers = []
+         for (var i=0; i<markers_Vale.length; i++) {
+           
+            var lat = markers_Vale[i][0];
+            var lon = markers_Vale[i][1];
+			var icn = markers_Vale[i][2];
+            var popupText = markers_Vale[i][3];
+            
+             var marker = new L.Marker([lat, lon], {icon: icn}).bindPopup(popupText);
+			arrValeMarkers.push(marker)
+         }
+		var markerLayerVale = L.layerGroup(arrValeMarkers).addTo(map);
+		
+		
+		//Loop through the North markers (declared on marker.js) array and add markers to layer
+		var arrNorthMarkers = []
+         for (var i=0; i<markers_North.length; i++) {
+           
+            var lat = markers_North[i][0];
+            var lon = markers_North[i][1];
+			var icn = markers_North[i][2];
+            var popupText = markers_North[i][3];
+            
+             var marker = new L.Marker([lat, lon], {icon: icn}).bindPopup(popupText);
+			arrNorthMarkers.push(marker)
+         }
+		var markerLayerNorth = L.layerGroup(arrNorthMarkers).addTo(map);		
+
+		//Loop through the Dorne markers (declared on marker.js) array and add markers to layer
+		var arrDorneMarkers = []
+         for (var i=0; i<markers_Dorne.length; i++) {
+           
+            var lat = markers_Dorne[i][0];
+            var lon = markers_Dorne[i][1];
+			var icn = markers_Dorne[i][2];
+            var popupText = markers_Dorne[i][3];
+            
+             var marker = new L.Marker([lat, lon], {icon: icn}).bindPopup(popupText);
+			arrDorneMarkers.push(marker)
+         }
+		var markerLayerDorne = L.layerGroup(arrDorneMarkers).addTo(map);	
+		
+
+		//Loop through the Reach markers (declared on marker.js) array and add markers to layer
+		var arrReachMarkers = []
+         for (var i=0; i<markers_Reach.length; i++) {
+           
+            var lat = markers_Reach[i][0];
+            var lon = markers_Reach[i][1];
+			var icn = markers_Reach[i][2];
+            var popupText = markers_Reach[i][3];
+            
+             var marker = new L.Marker([lat, lon], {icon: icn}).bindPopup(popupText);
+			arrReachMarkers.push(marker)
+         }
+		var markerLayerReach = L.layerGroup(arrReachMarkers).addTo(map);			
+		
+		//Loop through the II markers (declared on marker.js) array and add markers to layer
+		var arrIIMarkers = []
+         for (var i=0; i<markers_II.length; i++) {
+           
+            var lat = markers_II[i][0];
+            var lon = markers_II[i][1];
+			var icn = markers_II[i][2];
+            var popupText = markers_II[i][3];
+            
+             var marker = new L.Marker([lat, lon], {icon: icn}).bindPopup(popupText);
+			arrIIMarkers.push(marker)
+         }
+		var markerLayerII = L.layerGroup(arrIIMarkers).addTo(map);	
+		
+		//Loop through the Essos markers (declared on marker.js) array and add markers to layer
+		var arrEssosMarkers = []
+         for (var i=0; i<markers_Essos.length; i++) {
+           
+            var lat = markers_Essos[i][0];
+            var lon = markers_Essos[i][1];
+			var icn = markers_Essos[i][2];
+            var popupText = markers_Essos[i][3];
+            
+             var marker = new L.Marker([lat, lon], {icon: icn}).bindPopup(popupText);
+			arrEssosMarkers.push(marker)
+         }
+		var markerLayerEssos = L.layerGroup(arrEssosMarkers).addTo(map);	
+				
 		
 		// Hide markers if the user zooms out
 		map.on('zoomend', function () {
-			if (map.getZoom() <= 2 && map.hasLayer(markerLayer)) {
-				map.removeLayer(markerLayer);
+			if (map.getZoom() < 2) {
+				map.removeLayer(markerLayerSL);
+				map.removeLayer(markerLayerCL);
+				map.removeLayer(markerLayerVale);
+				map.removeLayer(markerLayerNorth);
+				map.removeLayer(markerLayerDorne);
+				map.removeLayer(markerLayerReach);				
+				map.removeLayer(markerLayerII);	
+				map.removeLayer(markerLayerEssos);	
 			}
-			if (map.getZoom() > 2 && map.hasLayer(markerLayer) == false)
+			else
 			{
-				map.addLayer(markerLayer);
+				map.addLayer(markerLayerSL);
+				map.addLayer(markerLayerCL);
+				map.addLayer(markerLayerVale);
+				map.addLayer(markerLayerNorth);
+				map.addLayer(markerLayerDorne);
+				map.addLayer(markerLayerReach);
+				map.addLayer(markerLayerII);
+				map.addLayer(markerLayerEssos);
+								
 			}   
 		}); 
 		
+		
+		function toggleSLLayer(){
+			if (map.hasLayer(markerLayerSL)) {
+				map.removeLayer(markerLayerSL);
+			}
+			else
+			{
+				map.addLayer(markerLayerSL);
+			}
+		}
+		
+		function toggleCLLayer(){
+			if (map.hasLayer(markerLayerCL)) {
+				map.removeLayer(markerLayerCL);
+			}
+			else
+			{
+				map.addLayer(markerLayerCL);
+			}
+		}
+		
+		function toggleValeLayer(){
+			if (map.hasLayer(markerLayerVale)) {
+				map.removeLayer(markerLayerVale);
+			}
+			else
+			{
+				map.addLayer(markerLayerVale);
+			}
+		}
+		
+		function toggleNorthLayer(){
+			if (map.hasLayer(markerLayerNorth)) {
+				map.removeLayer(markerLayerNorth);
+			}
+			else
+			{
+				map.addLayer(markerLayerNorth);
+			}
+		}				
+
+		function toggleDorneLayer(){
+			if (map.hasLayer(markerLayerDorne)) {
+				map.removeLayer(markerLayerDorne);
+			}
+			else
+			{
+				map.addLayer(markerLayerDorne);
+			}
+		}	
+
+		function toggleReachLayer(){
+			if (map.hasLayer(markerLayerReach)) {
+				map.removeLayer(markerLayerReach);
+			}
+			else
+			{
+				map.addLayer(markerLayerReach);
+			}
+		}	
+
+		function toggleIILayer(){
+			if (map.hasLayer(markerLayerII)) {
+				map.removeLayer(markerLayerII);
+			}
+			else
+			{
+				map.addLayer(markerLayerII);
+			}
+		}			
+		
+
+		function toggleEssosLayer(){
+			if (map.hasLayer(markerLayerEssos)) {
+				map.removeLayer(markerLayerEssos);
+			}
+			else
+			{
+				map.addLayer(markerLayerEssos);
+			}
+		}					
--- js/markers.js
+++ js/markers.js
@@ -10,10 +10,15 @@ var SLfort = L.icon({iconUrl: 'img/marker/SLfort.png',iconSize: [32, 74]})
 var SLtown = L.icon({iconUrl: 'img/marker/SLtown.png',iconSize: [32, 74]})
 
 var CL = L.icon({iconUrl: 'img/marker/CL.png',iconSize: [32, 74]})
+var CLtown = L.icon({iconUrl: 'img/marker/CLtown.png',iconSize: [32, 74]})
+var CLcity = L.icon({iconUrl: 'img/marker/CLcity.png',iconSize: [32, 74]})
+var CLfort = L.icon({iconUrl: 'img/marker/CLfort.png',iconSize: [32, 74]})
 
 var Dorne = L.icon({iconUrl: 'img/marker/Dorne.png',iconSize: [32, 74]})
 
 var II = L.icon({iconUrl: 'img/marker/II.png',iconSize: [32, 74]})
+var IItown = L.icon({iconUrl: 'img/marker/IItown.png',iconSize: [32, 74]})
+var IIcapitol = L.icon({iconUrl: 'img/marker/IIcapitol.png',iconSize: [32, 74]})
 
 var North = L.icon({iconUrl: 'img/marker/North.png',iconSize: [32, 74]})
 var NorthCity = L.icon({iconUrl: 'img/marker/NorthCity.png',iconSize: [32, 74]})
@@ -39,6 +44,10 @@ var SICity = L.icon({iconUrl: 'img/marker/SICity.png',iconSize: [32, 74]})
 var SITown = L.icon({iconUrl: 'img/marker/SITown.png',iconSize: [32, 74]})
 
 var Reach = L.icon({iconUrl: 'img/marker/Reach.png',iconSize: [32, 74]})
+var ReachFort = L.icon({iconUrl: 'img/marker/ReachFort.png',iconSize: [32, 74]})
+var ReachTown = L.icon({iconUrl: 'img/marker/ReachTown.png',iconSize: [32, 74]})
+var ReachCity = L.icon({iconUrl: 'img/marker/ReachCity.png',iconSize: [32, 74]})
+var ReachCapitol = L.icon({iconUrl: 'img/marker/ReachCapitol.png',iconSize: [32, 74]})
 
 var RL = L.icon({iconUrl: 'img/marker/RL.png',iconSize: [32, 74]})
 
@@ -55,13 +64,7 @@ var Wall = L.icon({iconUrl: 'img/marker/Wall.png',iconSize: [32, 74]})
 var West = L.icon({iconUrl: 'img/marker/West.png',iconSize: [32, 74]})
 
 // Define the markers
-var markers = [
-	// Each marker will be a line with the following format:
-	//		[Lat, Long, icon, 'popup']
-	// "Lat" and "Long" are the two numbers obtained by clicking on the map.
-	// icon is one of the names defined above
-	// popup is the text that will display on screen
-//Stormlands
+var markers_SL = [
 [-31.428663117358596,-114.697265625,SL,"Lover's Hill"],
 [-32.76880048488168,-116.27929687499999,SL,"Nightsong"],
 [-29.228890030194215,-107.841796875,SL,"Harvest Hall"],
@@ -87,8 +90,10 @@ var markers = [
 [-33.06392419812064,-100.8984375,SLfort,"Caelen's Reach"],
 [-11.350796722383672,-76.552734375,SLfort,"Durran's Grief"],
 [-11.95334939364343,-86.923828125,SLfort,"Forrestwatch"],
-[-29.228890030194215,-97.646484375,SL,"Gallowsgrey"],
-//Vale
+[-29.228890030194215,-97.646484375,SL,"Gallowsgrey"]]
+
+
+var markers_Vale = [
 [27.21555620902969,-88.06640625,Vale,"Gates of the Moon"],
 [28.844673680771795,-87.71484375,Valecapitol,"The Eyrie"],
 [25.085598897064777,-92.197265625,Vale,"The Bloody Gate"],
@@ -115,8 +120,9 @@ var markers = [
 [25.878994400196202,-77.6953125,Vale,"Grey Glen"],
 [24.367113562651276,-81.474609375,Vale,"Redfort"],
 [21.94304553343818,-78.046875,Valefort,"Little Redfort"],
-[27.371767300523047,-83.583984375,Vale,"Hardhold"],
-//North
+[27.371767300523047,-83.583984375,Vale,"Hardhold"]]
+
+var markers_North = [
 [47.040182144806664,-112.763671875,North,"Greywater Watch"],
 [51.45400691005982,-107.40234375,North,"Moat Cailin"],
 [50.56928286558243,-94.39453125,North,"Oldcastle"],
@@ -143,9 +149,105 @@ var markers = [
 [71.32895017791999,-117.94921874999999,North,"Ironrath"],
 [73.89811065820952,-128.759765625,North,"Bear Island"],
 [47.45780853075031,-138.779296875,North,"Flint's Finger"],
-[53.69670647530323,-134.47265625,NorthFort,"Ironguard"],
+[53.69670647530323,-134.47265625,NorthFort,"Ironguard"]]
+
+var markers_CL = [
+[0.9667509997666425,-73.916015625,CL,"Sharp Point"],
+[-1.4061088354351468,-72.50976562499999,CL,"Stonedance"],
+[-6.839169626342808,-81.2109375,CLtown,"Wendwater Town"],
+[-10.40137755454354,-92.021484375,CL,"Kingswood Hall"],
+[7.27529233637217,-73.388671875,CL,"Dragonstone"],
+[5.266007882805511,-76.46484375,CLtown,"Driftmark"],
+[4.477856485570586,-78.134765625,CLfort,"Aelinorfort"],
+[12.382928338487408,-73.740234375,CL,"Dyre Den"],
+[14.349547837185375,-68.291015625,CL,"Claw Isle"],
+[4.653079918274051,-80.68359375,CL,"Sweetport Sound"],
+[8.494104537551882,-82.353515625,CL,"Rook's Rest"],
+[6.751896464843375,-87.1875,CL,"Axefall"],
+[9.795677582829743,-92.548828125,CL,"Antlers"],
+[2.460181181021018,-85.60546875,CLtown,"Duskendale"],
+[-1.9332268264771106,-86.396484375,CL,"Knight's Shore"],
+[14.774882506516272,-74.35546875,CL,"Brownhollow"],
+[2.6357885741666065,-93.955078125,CL,"Stokeworth"],
+[-0.7031073524364783,-95.361328125,CL,"Hayford"],
+[-1.5818302639606328,-91.49414062499999,CL,"Rosby"],
+[-4.915832801313164,-99.31640625,CLfort,"Fort Rosby"],
+[-4.8282597468669755,-94.21875,CLcity,"King's Landing"]]
+//Reach
+
+var markers_Reach = [
+[-4.12728532324537,-106.083984375,Reach,"Darkdell"],
+[-10.487811882056695,-105.029296875,ReachTown,"Tumbleton"],
+[-7.18810087117902,-116.630859375,Reach,"The Ring"],
+[-13.923403897723334,-110.0390625,ReachTown,"Bitterbridge"],
+[-17.30868788677001,-109.77539062499999,Reach,"Grassfield Keep"],
+[-10.574222078332806,-124.01367187499999,Reach,"Coldmoat"],
+[-16.55196172197251,-130.341796875,Reach,"Goldengrove"],
+[-14.179186142354169,-138.69140625,Reach,"Red Lake"],
+[-18.396230138028812,-146.337890625,Reach,"Old Oak"],
+[-22.024545601240327,-137.197265625,Reach,"Appleton"],
+[-25.085598897064763,-141.064453125,ReachTown,"Shield Islands"],
+[-25.48295117535531,-134.208984375,Reach,"New Barrel"],
+[-20.550508894195637,-116.27929687499999,Reach,"Longtable"],
+[-27.059125784374054,-113.5546875,ReachTown,"Ashford"],
+[-24.206889622398023,-120.84960937499999,Reach,"Cider Hall"],
+[-29.228890030194215,-130.078125,ReachCapitol,"Highgarden"],
+[-32.175612478499325,-124.01367187499999,Reach,"Starpike"],
+[-36.38591277287651,-130.166015625,Reach,"Horn Hill"],
+[-33.65120829920497,-148.88671874999997,Reach,"Bandallon"],
+[-34.74161249883172,-140.361328125,Reach,"Brightwater Keep"],
+[-38.06539235133248,-140.9765625,Reach,"Honeyholt"],
+[-40.78054143186031,-138.427734375,Reach,"Uplands"],
+[-41.771311679764054,-143.7890625,ReachCity,"Oldtown"],
+[-51.39920565355377,-142.646484375,Reach,"Sunhouse"],
+[-43.771093817756494,-148.798828125,Reach,"Blackcrown"],
+[-51.67255514839674,-150.556640625,ReachTown,"The Arbor"],
+[-45.89000815866183,-148.18359375,Reach,"Three Towers"]]
+//Iron Islands
+var markers_II = [
+[33.211116472416855,-138.33984375,II,"Blacktyde Hall"],
+[31.203404950917395,-135.87890625,II,"Volmark"],
+[27.527758206861886,-137.28515624999997,II,"Grey Garden"],
+[30.977609093348686,-140.888671875,II,"Orkmont"],
+[30.600093873550072,-145.72265625,II,"Castle Drumm"],
+[31.728167146023935,-147.91992187499997,II,"Carved Keep"],
+[30.90222470517144,-149.677734375,II,"Reaver's Rest"],
+[29.152161283318915,-143.7890625,II,"Pebbleton Tower"],
+[27.371767300523047,-141.943359375,II,"Iron Holt"],
+[25.878994400196202,-144.228515625,IItown,"Lordsport"],
+[26.03704188651584,-146.42578125,II,"Saltcliffe"],
+[31.203404950917395,-175.25390624999997,II,"Lonely Light"],
+[29.53522956294847,-136.7578125,II,"Skyshatter"],
+[28.92163128242129,-134.736328125,II,"Twenty Towers"],
+[30.600093873550072,-139.482421875,II,"Nettle's Scourge"],
+[26.03704188651584,-142.11914062499997,II,"Pyke"],
+[29.611670115197406,-148.271484375,IIcapitol,"Hammerhorn"],
+[28.613459424004414,-146.513671875,II,"Depth's Lamment"]]
+
+var markers_Dorne = [
+[-36.31512514748051,-99.66796875,Dorne,"Wyl"],
+[-36.31512514748051,-106.61132812499999,Dorne,"Vulture's Roost"],
+[-39.571822237343724,-115.400390625,Dorne,"Kingsgrave"],
+[-43.32517767999295,-113.5546875,Dorne,"Skyreach"],
+[-43.19716728250127,-102.83203125,Dorne,"Yronwood"],
+[-40.51379915504413,-95.712890625,Dorne,"Gallowsgrey"],
+[-45.33670190996812,-88.59374999999999,Dorne,"The Tor"],
+[-46.01222384063236,-71.806640625,Dorne,"Ghost Hill"],
+[-48.80686346108517,-68.291015625,Dorne,"Spottswood"],
+[-50.736455137010644,-71.806640625,Dorne,"Sunspear"],
+[-53.4357192066942,-78.75,Dorne,"Lemonwood"],
+[-49.439556958940834,-86.1328125,Dorne,"Godsgrace"],
+[-48.74894534343292,-92.10937499999999,Dorne,"Scorched Rock"],
+[-53.067626642387374,-89.12109375,Dorne,"Salt Shore"],
+[-50.34546040860479,-96.85546875,Dorne,"Vaith"],
+[-50.62507306341435,-109.86328125,Dorne,"Hellholt"],
+[-50.457504020420565,-122.87109375,Dorne,"Sandstone"],
+[-40.44694705960048,-126.03515625,Dorne,"Blackmont"],
+[-43.644025847699496,-126.03515625,Dorne,"High Hermitage"],
+[-47.5172006978394,-129.375,Dorne,"Starfall"]]
 
 // Wall
+var markers_Essos = [
 [77.3318091852135,-93.95507879555225,Wall,"Castle Black"],
 [77.35106958476409,-85.16601629555225,Wall,"Eastwatch"],
 [77.2544787022853,-103.00781317055225,Wall,"Shadow Tower"],
@@ -162,14 +264,6 @@ var markers = [
 [-33.797408767572485,-54.404296875,EssosBigCity,"Tyrosh"],
 [36.24427318493909,-21.97265625,EssosBigCity,"Lorath"],
 [-51.72702815704775,5.625,EssosBigCity,"Volantis"],
-
-//Old Valyria
-[-52.908902047770255,46.142578125,ValyriaRuin,"Elyria"],
-[-61.8146638946839,60.99609375,ValyriaRuin,"Velos"],
-[-57.23150299147891,59.32617187499999,ValyriaRuin,"Ghozai"],
-[-67.67608458198097,35.68359375,ValyriaRuin,"Tyria"],
-[-68.91100484562018,21.533203125,ValyriaRuin,"Oros"],
-[-71.01695975726373,31.904296874999996,ValyriaRuin,"Valyria"],
 
 //// Rest of Essos
 [1.3182430568620136,-28.125,EssosRuin,"Ghoyan Drohe"],
@@ -196,7 +290,7 @@ var markers = [
 [-48.63290858589533,21.181639954447746,ValyriaRuin,"Ruins"],
 [-51.835777520452474,52.82226495444774,EssosCity,"Tolos"],
 [-66.687783861162,82.61718682944775,EssosRuin,"Old Ghis"],
-[-73.022591571473,86.57226495444775,EssosCity,"New Ghis"],
+[-73.022591571473,86.57226495444775,EssosBigCity,"New Ghis"],
 [-57.32652122521709,78.57421807944775,EssosBigCity,"Astapor"],
 [-45.95114968669139,82.52929620444775,EssosBigCity,"Yunkai"],
 [-40.913512576127566,89.64843682944775,EssosBigCity,"Meereen"],
@@ -219,22 +313,29 @@ var markers = [
 [-40.380028402511826,-4.218750670552254,EssosTown,"Selhorys"],
 [-45.151053265563384,-4.218750670552254,EssosTown,"Valysar"],
 [-48.6909603909255,-2.0214850455522537,EssosTown,"Volon Therys"],
-// Summer Islands
-[-77.80477074199555,-46.757813170552254,SITown,"Tall Trees Town"],
-[-76.41397301513199,-56.601563170552254,SICity,"Lotus Port"],
-[-74.54332982677904,-65.39062567055225,SITown,"Last Lament"],
-
-
-// Sothoryos 
-[-80.8588748843299,74.619140625,SothoryosRuin,"Zamettar"],
-[-83.03688198552364,81.5625,SothoryosRuin,"Yeen"],
-[-82.48333497678894,62.75390625,SothoryosRuin,"Gogossos"],
-[-79.68718415450823,132.099609375,SothoryosRuin,"Gorosh"],
-
 // Moraq
 [-76.43460358351301,158.3789049088955,EssosTown,"Vahar"],
 [-73.52839948765174,161.3671861588955,EssosTown,"Faros"],
-[-80.61842419685506,179.1210924088955,EssosTown,"Port Moraq"]
+[-80.61842419685506,179.1210924088955,EssosTown,"Port Moraq"]]
 
+// Summer Islands
+var markers_SI = [
+[-77.80477074199555,-46.757813170552254,SITown,"Tall Trees Town"],
+[-76.41397301513199,-56.601563170552254,SICity,"Lotus Port"],
+[-74.54332982677904,-65.39062567055225,SITown,"Last Lament"]]
 
-]
+//Old Valyria
+var markers_Valyria = [
+[-52.908902047770255,46.142578125,ValyriaRuin,"Elyria"],
+[-61.8146638946839,60.99609375,ValyriaRuin,"Velos"],
+[-57.23150299147891,59.32617187499999,ValyriaRuin,"Ghozai"],
+[-67.67608458198097,35.68359375,ValyriaRuin,"Tyria"],
+[-68.91100484562018,21.533203125,ValyriaRuin,"Oros"],
+[-71.01695975726373,31.904296874999996,ValyriaRuin,"Valyria"]]
+
+// Sothoryos 
+var markers_Sothoryos = [
+[-80.8588748843299,74.619140625,SothoryosRuin,"Zamettar"],
+[-83.03688198552364,81.5625,SothoryosRuin,"Yeen"],
+[-82.48333497678894,62.75390625,SothoryosRuin,"Gogossos"],
+[-79.68718415450823,132.099609375,SothoryosRuin,"Gorosh"]]
