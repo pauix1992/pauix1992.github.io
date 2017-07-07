4d82c6e3b798316010d2a54d9742a46c7ceb6e11
Pau Amich <pamich1@gmail.com>
2017-07-07 23:43:05 +0200

    Markers 2.0

---
+++ new/js/markers.js
@@ -0,0 +1,14 @@
+  /********************************************/
+ /**				MARKERS					**/
+/********************************************/
+
+// Define marker icons
+// For each region add an image to 'img/marker/' 
+var Keep = L.icon({iconUrl: 'img/marker/keep_4.png',iconSize: [12, 18]})
+
+
+var keeps = [
+[-78.24243607612065,-82.353515625,Keep,"Yronwood"],
+]
+
+var markers_Logos = []
