8ac07bdc09478321970a63c03a893f23c89c244d
Pau Amich <pamich1@gmail.com>
2017-07-08 12:55:43 +0200

    tyhj

--- new/js/markers.js
+++ new/js/markers.js
@@ -8,7 +8,7 @@ var Keep_4 = L.icon({iconUrl: 'img/marker/keep_4.png',iconSize: [12, 18]})
 var Keep_3 = L.icon({iconUrl: 'img/marker/keep_4.png',iconSize: [6, 9]})
 
 
-var keeps_4 = [
+var keeps = [
 [-78.24243607612065,-82.353515625,"Yronwood"],
 ]
 
