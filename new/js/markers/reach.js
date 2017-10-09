245d3408db0e517933e05a6d136f0d036ef57fb4
pamichv <pamichv@uoc.edu>
2017-10-09 17:05:47 +0200

    Arbor Test
    Arbor Test

--- new/js/markers/reach.js
+++ new/js/markers/reach.js
@@ -37,16 +37,30 @@ var ReachPort_2 = L.icon({iconUrl: 'img/marker/Reach/searesource.png',iconSize: 
 
 
 var reachKeeps = [
-
+[-83.59903070836198,-147.99525707960126,"Starfish Harbor","Port_4","Port_3","Port_2"],
+[-83.9050579559856,-146.7647883296013,"Vinetown","Keep_4","Keep_3","Keep_2"],
+[-83.70591605135859,-143.6886164546013,"Ryamsport","Town_4","Town_3","Town_2"],
+[-83.49033793795238,-143.6007258296013,"P","Port_4","Port_3","Port_2"],
 ]
 
 
 var reachVillages = [
-
+    [-84.07074698172724,-146.4132258296013,"V","Village_4","Village_3","Village_2"],
+    [-83.97925949886205,-145.0069758296013,"V","Village_4","Village_3","Village_2"],
+    [-83.79204408779539,-142.3702570796013,"V","Village_4","Village_3","Village_2"],
+    [-83.69627310497607,-146.5890070796013,"V","Village_4","Village_3","Village_2"],
+    [-83.69627310497607,-149.4015070796013,"V","Village_4","Village_3","Village_2"],
+    [-83.28798543651806,-146.5890070796013,"V","Village_4","Village_3","Village_2"],
+    [-83.32895075967835,-149.6651789546013,"V","Village_4","Village_3","Village_2"],
+    [-83.40004205976699,-150.7198664546013,"V","Village_4","Village_3","Village_2"],
 ]
 
 var reachResources = [
-
+    [-83.59903070836198,-144.9190852046013,"R","LandResource_4","LandResource_3","LandResource_2"],
 ]
 
-alert("REACH LOADED !!!!"); 
+
+
+
+
+
